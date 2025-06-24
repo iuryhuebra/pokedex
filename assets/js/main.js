cbeaff69f1948b6351555c62aa32d3b6ba227523
iuryhuebra <iuryhuebra702@gmail.com>
2025-06-23 22:47:10 -0300

    Pokedex DEV #7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,8 @@
-import { getPokemons, limit } from "./poke-api.js";
+import { getPokemons } from "./poke-api.js";
 
-export let offset = 0
+
 
 getPokemons(0)
 
-document.querySelectorAll('.page-control li').forEach((element) => {
-    element.onclick = () => {
-        document.querySelector('.page-control li.clicked').classList.remove('clicked')
-        element.classList.add('clicked')
-        offset = (element.textContent * limit) - limit
 
-        getPokemons(offset)
-    }
-})
 
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,6 @@
-import { offset } from "./main.js"
 
-export let limit = document.getElementById('limit').value
-
+let limit = document.getElementById('limit').value
+let offset = 0
 
 document.getElementById('limit').onchange = () => {    
     limit = document.getElementById('limit').value
@@ -28,10 +27,28 @@ function converterPokemonLi(pokemonDetail) {
                         }).join('')
                     }
                 </ol>
-                <img src="${img}" id="${name}Img" alt="${name}">
+                ${img ? `<img src="${img}" id="${name}Img" alt="${name}">` : ''}
             </div>
         </li>
     `
+}
+
+function createPageList(value) {
+    const li = document.createElement('li')
+    li.textContent = value
+    return li
+}
+
+function addEventsPageControl() {
+    document.querySelectorAll('.page-control li').forEach((element) => {
+        element.onclick = () => {
+            document.querySelector('.page-control li.clicked').classList.remove('clicked')
+            element.classList.add('clicked')
+            offset = (element.textContent * limit) - limit
+    
+            getPokemons(offset)
+        }
+    })
 }
   
 export async function getPokemons(offset) {
@@ -40,11 +57,12 @@ export async function getPokemons(offset) {
     const responseJson = await response.json()
     const pokemons = responseJson.results
     const pokemonList = document.getElementById('pokemonList');
+    
 
     document.querySelectorAll('.pokemons li').forEach((element) => {
         element.remove()
     })
-    
+        
     const pokemonsDetails = await Promise.all(
         pokemons.map((pokemon) => (
             fetch(pokemon.url)
@@ -58,4 +76,52 @@ export async function getPokemons(offset) {
         pokemonList.innerHTML += converterPokemonLi(detail)
     })
     
+    const pageSelected = Number(document.querySelector('.clicked').textContent) ?? 1
+    
+    document.querySelectorAll('.page-control li').forEach((element) => element.remove())
+    const pageLimit = Math.ceil(responseJson.count / limit)
+    let pages = Array.from({ length: pageLimit }, (_, i) => i + 1)
+    const pageControl = document.querySelector('.page-control')
+    let pageLi
+
+    pages.forEach((page) => {
+        switch (page) {
+            case pageSelected:
+                pageLi = createPageList(page)                
+                pageLi.classList.add('clicked')
+                break
+            
+            case 1:
+                pageLi = createPageList(page)
+                break
+                
+            case pageLimit:
+                pageLi = createPageList(page)
+                break
+            
+            case pageSelected - 1:
+                pageLi = createPageList(page)                
+                break
+            
+            case pageSelected - 2:
+                pageLi = createPageList(page)                
+                break
+            
+            case pageSelected + 1:
+                pageLi = createPageList(page)                
+                break
+            
+            case pageSelected + 2:
+                pageLi = createPageList(page)                
+                break
+
+            default:
+                break
+        }
+        
+        pageControl.appendChild(pageLi)
+    })
+    
+    
+    addEventsPageControl()
 }
